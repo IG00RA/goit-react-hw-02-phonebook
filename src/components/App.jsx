5cb2ef8ac9c245e2211fb2aecf2c4e34c11b2bac
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-04-27 07:46:17 +0300

    add style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyles';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    const normalizeName = newContact.name.toLowerCase();
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === normalizeName
+      )
+    ) {
+      return alert(`${newContact.name} is already in contact list`);
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  findContact = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  render() {
+    const {
+      addContact,
+      deleteContact,
+      getVisibleContacts,
+      findContact,
+      state,
+    } = this;
+
+    return (
+      <>
+        <GlobalStyle />
+        <h1>Phonebook</h1>
+        <ContactForm onAdd={addContact} />
+        <h2>Contacts</h2>
+        <Filter onChange={findContact} value={state.filter} />
+        <ContactList
+          getContacts={getVisibleContacts()}
+          onDelete={deleteContact}
+        />
+      </>
+    );
+  }
+}
